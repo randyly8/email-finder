b79194762410431855d8db8562b54dde8753b29b
randyly8 <RaNdYlY8@gmail.com>
2020-05-23 15:00:08 -0500

    commit

--- src/test/java/edu/depaul/email/emailFinderTest.java
+++ src/test/java/edu/depaul/email/emailFinderTest.java
@@ -14,35 +14,80 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class emailFinderTest {
     @Test
-    @DisplayName("Default Valid URL")
+    @DisplayName("Valid URL")
     void ValidURLTest() {
         EmailFinder finder = new EmailFinder();
         String[] url = {"validEmailForTesting"};
         finder.run(url);
-        String path = "email.txt";
-        String email;
-        try (Stream<String> lines = Files.lines(Paths.get(path))){
+        String path = "good-links.txt";
+        try (Stream<String> lines = Files.lines(Paths.get(path))) {
             String content = lines.collect(Collectors.joining(System.lineSeparator()));
-            email = content;
-            assertEquals(email,"bsatterfield@yahoo.com");
-        } catch (Exception e) {}
+            assertEquals(content, "validEmailForTesting");
+        } catch (IOException e) {e.printStackTrace();}
     }
 
     @Test
     @DisplayName("Invalid URL with length greater than 1")
     void InvalidURLFormatTest() {
-        EmailFinder.main(new String[] {"qwerty://cdm.depaul.edu"});
+        EmailFinder finder = new EmailFinder();
+        String[] url = {"nonexistentPath"};
+        finder.run(url);
+        String path = "badlinks.txt";
+        try (Stream<String> lines = Files.lines(Paths.get(path))) {
+            String content = lines.collect(Collectors.joining(System.lineSeparator()));
+            assertEquals(content, "nonexistentPath");
+        } catch (IOException e) {e.printStackTrace();}
     }
 
     @Test
     @DisplayName("Invalid URL with length less than 1")
     void URLOfSizeLessThan1Test() {
-        EmailFinder.main(new String[] {""});
+        EmailFinder finder = new EmailFinder();
+        String[] url = {""};
+        finder.run(url);
+        String path = "badlinks.txt";
+        try (Stream<String> lines = Files.lines(Paths.get(path))) {
+            String content = lines.collect(Collectors.joining(System.lineSeparator()));
+            assertEquals(content, "");
+        } catch (IOException e) {e.printStackTrace();}
     }
 
     @Test
     @DisplayName("Null URL")
     void nullURLTest() {
-        EmailFinder.main(new String[] {});
+        EmailFinder finder = new EmailFinder();
+        String[] url = {};
+        finder.run(url);
+        String path = "badlinks.txt";
+        try (Stream<String> lines = Files.lines(Paths.get(path))) {
+            String content = lines.collect(Collectors.joining(System.lineSeparator()));
+            assertEquals(content, "");
+        } catch (IOException e) {e.printStackTrace();}
+    }
+
+    @Test
+    @DisplayName("Valid URL with valid email")
+    void validEmailTest() {
+        EmailFinder finder = new EmailFinder();
+        String[] url = {"validEmailForTesting"};
+        finder.run(url);
+        String path = "email.txt";
+        try (Stream<String> lines = Files.lines(Paths.get(path))) {
+            String content = lines.collect(Collectors.joining(System.lineSeparator()));
+            assertEquals(content, "bsatterfield@yahoo.com");
+        } catch (IOException e) {e.printStackTrace();}
+    }
+
+    @Test
+    @DisplayName("Valid URL with invalid email")
+    void invalidEmailTest() {
+        EmailFinder finder = new EmailFinder();
+        String[] url = {"invalidEmailForTesting"};
+        finder.run(url);
+        String path = "email.txt";
+        try (Stream<String> lines = Files.lines(Paths.get(path))) {
+            String content = lines.collect(Collectors.joining(System.lineSeparator()));
+            assertEquals(content, "");
+        } catch (IOException e) {e.printStackTrace();}
     }
 }
