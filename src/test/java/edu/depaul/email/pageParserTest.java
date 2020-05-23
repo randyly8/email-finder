611506ac4e74b6988c87c81a61aedecade3495b0
randyly8 <RaNdYlY8@gmail.com>
2020-05-23 17:53:18 -0500

    commit

--- src/test/java/edu/depaul/email/pageParserTest.java
+++ src/test/java/edu/depaul/email/pageParserTest.java
@@ -1,44 +1,72 @@
 package edu.depaul.email;
 
 import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class pageParserTest {
+    String validEmails = "[earmstro@yahoo.com, kronvold@gmail.com, barjam@comcast.net, seanq@optonline.net, north@icloud.com, demmel@att.net, sthomas@hotmail.com, novanet@outlook.com, miltchev@gmail.com, fallorn@gmail.com, citadel@yahoo.com, grolschie@gmail.com]";
+    String validLinks = "[http://www.youtube.com, http://www.facebook.com, http://www.baidu.com, http://www.yahoo.com, http://www.amazon.com, http://www.wikipedia.org, http://www.qq.com, http://www.google.com, http://www.twitter.com, http://www.live.com]";
     @Test
-    @DisplayName("Find email test on valid URL")
+    @DisplayName("Find (1) email")
     void findEmailTest() {
         PageFetcher fetcher = new PageFetcher();
         Document doc = fetcher.get("validEmailForTesting");
         PageParser parser = new PageParser();
-        parser.findEmails(doc);
+        Set<String> content = parser.findEmails(doc);
+        assertEquals(content.toString(), "[bsatterfield@yahoo.com]");
     }
     @Test
-    @DisplayName("Find email test on valid URL")
-    void findEmailInvalidTest() {
+    @DisplayName("Find emails")
+    void findMultipleEmailsTest() {
         PageFetcher fetcher = new PageFetcher();
-        Document doc = fetcher.get("invalidEmailForTesting");
+        Document doc = fetcher.get("multipleValidEmailsForTesting.txt");
         PageParser parser = new PageParser();
-        parser.findEmails(doc);
+        Set<String> content = parser.findEmails(doc);
+        assertEquals(content.toString(), validEmails);
     }
-
     @Test
-    @DisplayName("Find link test on invalid URL")
-    void findLinksTest() {
+    @DisplayName("Find valid email in list of invalid emails and links")
+    void findEmailInRandomListTest() {
+        PageFetcher fetcher = new PageFetcher();
+        Document doc = fetcher.get("valid-Invalid-emails-links.txt");
+        PageParser parser = new PageParser();
+        Set<String> content = parser.findEmails(doc);
+        assertEquals(content.toString(), "[seanq@optonline.net]");
+    }
+    @Test
+    @Disabled
+    @DisplayName("Find (1) link")
+    void findLinkTest() {
         PageFetcher fetcher = new PageFetcher();
         Document doc = fetcher.get("validURLForTesting");
         PageParser parser = new PageParser();
-        parser.findLinks(doc);
+        Set<String> content = parser.findLinks(doc);
+        assertEquals(content.toString(), "[https://d2l.depaul.edu/d2l/home]");
     }
     @Test
-    @DisplayName("Find link test on invalid URL")
-    void findLinksInvalidTest() {
+    @Disabled
+    @DisplayName("Find links")
+    void findMultipleLinksTest() {
         PageFetcher fetcher = new PageFetcher();
-        Document doc = fetcher.get("invalidURLForTesting");
+        Document doc = fetcher.get("multipleValidURLsForTesting.txt");
         PageParser parser = new PageParser();
-        parser.findLinks(doc);
+        Set<String> content = parser.findLinks(doc);
+        assertEquals(content.toString(), validLinks);
+    }
+    @Test
+    @Disabled
+    @DisplayName("Find valid link in list of invalid emails and links")
+    void findLinkInRandomListTest() {
+        PageFetcher fetcher = new PageFetcher();
+        Document doc = fetcher.get("valid-Invalid-emails-links.txt");
+        PageParser parser = new PageParser();
+        Set<String> content = parser.findLinks(doc);
+        assertEquals(content.toString(), "[http://www.youtube.com]");
     }
 }
