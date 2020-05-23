f0dcf7bc9659eb8b9b59faad63c9f45c1370d194
randyly8 <RaNdYlY8@gmail.com>
2020-05-23 14:11:58 -0500

    commit

--- src/test/java/edu/depaul/email/emailFinderTest.java
+++ src/test/java/edu/depaul/email/emailFinderTest.java
@@ -3,12 +3,29 @@ package edu.depaul.email;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class emailFinderTest {
     @Test
     @DisplayName("Default Valid URL")
     void ValidURLTest() {
-        EmailFinder.main(new String[] {"http://cdm.depaul.edu"});
+        EmailFinder finder = new EmailFinder();
+        String[] url = {"validEmailForTesting"};
+        finder.run(url);
+        String path = "email.txt";
+        String email;
+        try (Stream<String> lines = Files.lines(Paths.get(path))){
+            String content = lines.collect(Collectors.joining(System.lineSeparator()));
+            email = content;
+            assertEquals(email,"bsatterfield@yahoo.com");
+        } catch (Exception e) {}
     }
 
     @Test
