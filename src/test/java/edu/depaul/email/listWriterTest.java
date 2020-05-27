00707629a2cdb2fa7639707939c53888433bf537
randyly8 <RaNdYlY8@gmail.com>
2020-05-27 16:15:42 -0500

    commit

--- src/test/java/edu/depaul/email/listWriterTest.java
+++ src/test/java/edu/depaul/email/listWriterTest.java
@@ -3,9 +3,21 @@ package edu.depaul.email;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class listWriterTest {
     @Test
     @DisplayName("Constructor Test")
     void constructorTest() {
+        OutputStream stream = null;
+        try {
+            stream = new FileOutputStream("empty.txt");
+        } catch (FileNotFoundException e) {e.printStackTrace();}
+        ListWriter list = new ListWriter(stream);
+        assertNotNull(list);
     }
 }
